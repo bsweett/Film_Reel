473e626f542375ac9b2888cfeb1c0784e4ea7ec3
Brayden <braydengirard@Braydens-MacBook-Pro.local>
2013-10-19 16:11:03 -0400

    Fixed all the files again

---
+++ Film_Reel/SignUpEmailController.m
@@ -0,0 +1,38 @@
+//
+//  SignUpEmailController.m
+//  Film_Reel
+//
+//  Created by Brayden Girard on 2013-10-19.
+//  Copyright (c) 2013 Ben Sweett (100846396) and Brayden Girard (100852106). All rights reserved.
+//
+
+#import "SignUpEmailController.h"
+
+@interface SignUpEmailController ()
+
+@end
+
+@implementation SignUpEmailController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
