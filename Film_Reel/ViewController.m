b593e9b58084f57c4cc177c8bdd8dbac0a546959
Ben Sweett <bensweett@cmail.carleton.ca>
2013-10-19 17:27:02 -0400

    Added some validation checks for user login
    also added some basic error handling before any networking is done

--- Film_Reel/ViewController.m
+++ Film_Reel/ViewController.m
@@ -8,15 +8,24 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+
+@interface ViewController () <UITextFieldDelegate>
 
 @end
 
+
 @implementation ViewController
+
+@synthesize usernameField;
+@synthesize passwordField;
+@synthesize error;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    usernameField.delegate = self;
+    passwordField.delegate = self;
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -28,12 +37,80 @@
 
 -(IBAction)doLoginButton:(id)sender
 {
+    NSString * username = usernameField.text;
+    NSString * password = passwordField.text;
+    NSString * errorMessage = @"";
 
+    
+    if(![username  isEqual: @""] && ![password  isEqual: @""])
+    {
+        if([self validateUserNameWithString:username] == TRUE && [self validatePasswordWithString:password] == TRUE)
+        {
+            [self performSegueWithIdentifier:@"loggedIn" sender:sender];
+                //Start Network operations method
+                // inside that method handle networking errors and invavlid user and password errors
+                // When the networking operations finish return a BOOLEAN Value to start the modal to main app view
+        } else
+        {
+            errorMessage = @"Password or Username are the wrong length or contain invalid characters";
+            error = [[UIAlertView alloc] initWithTitle: @"Login Error" message: errorMessage delegate: self cancelButtonTitle: @"Ok" otherButtonTitles: nil];
+            [error show];
+        }
+    } else
+    {
+        errorMessage = @"Please fill in the required fields";
+        error = [[UIAlertView alloc] initWithTitle: @"Login Error" message: errorMessage delegate: self cancelButtonTitle: @"Ok" otherButtonTitles: nil];
+        [error show];
+    }
+    
+    
 }
 
 -(IBAction)doCreateButton:(id)sender
 {
     
+}
+
+- (BOOL) textFieldShouldReturn:(UITextField*) textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+
+// Validations --------------------------------------------------------------------
+- (BOOL)validateUserNameWithString:(NSString*)username
+{
+    if( username.length > MIN_ENTRY_SIZE && username.length < MAX_USERNAME_ENTRY )
+    {
+        NSString *nameRegex = @"[A-Z0-9a-z]*";
+        NSPredicate *nameTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", nameRegex];
+        return [nameTest evaluateWithObject:username];
+    } else
+    {
+        return FALSE;
+    }
+}
+
+- (BOOL)validatePasswordWithString:(NSString*)pass
+{
+    if( pass.length > MIN_ENTRY_SIZE && pass.length < MAX_PASSWORD_ENTRY )
+    {
+        NSString *passwordRegex = @"^[a-zA-Z_0-9\\-_,;.:#+*?=!§$%&/()@]+$";
+        NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", passwordRegex];
+        return [emailTest evaluateWithObject:pass];
+    } else
+    {
+        return FALSE;
+    }
+}
+
+
+- (BOOL)validateEmailWithString:(NSMutableString*)emailaddress
+{
+    NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
+    return [emailTest evaluateWithObject:emailaddress];
 }
 
 @end
