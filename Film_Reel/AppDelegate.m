d44a324fbe7b8816c39c3ad602d96510580a5079
Ben Sweett <bensweett@cmail.carleton.ca>
2013-12-03 17:12:08 -0500

    Removed nav bar and fix login screen

--- Film_Reel/AppDelegate.m
+++ Film_Reel/AppDelegate.m
@@ -36,10 +36,9 @@
 // Override point for customization after application launch.
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    //[[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
-    //[[UINavigationBar appearance] setTintColor:[[UIColor alloc] initWithRed:0 green:154 blue:255 alpha:1]];
-    //[[UINavigationBar appearance] setBackIndicatorImage:[UIImage imageNamed:@"backbutton.png"]];
-    //[[UINavigationBar appearance] setBackIndicatorTransitionMaskImage:[UIImage imageNamed:@"backbutton.png"]];
+    [[UINavigationBar appearance] setBarTintColor:[[UIColor alloc] initWithRed:0 green:154 blue:255 alpha:1]];
+    [[UINavigationBar appearance] setBackIndicatorImage:[UIImage imageNamed:@"backbutton.png"]];
+    [[UINavigationBar appearance] setBackIndicatorTransitionMaskImage:[UIImage imageNamed:@"backbutton.png"]];
     return YES;
 }
 
